71a5bab2d4486f1e3e4ec8ef651d67f682c903e3
heineeg <32772232+heineeg@users.noreply.github.com>
2017-12-26 16:00:25 +0100

    * change the buttons to start a new game

--- js/page_manager.js
+++ js/page_manager.js
@@ -1,6 +1,7 @@
 var ruleslink = document.getElementById('rules');
 var leaderboardlink = document.getElementById('leaderboard');
 var infolink = document.getElementById('info');
+var contentbox;
 
 ruleslink.addEventListener('click', openrules, false);
 leaderboardlink.addEventListener('click', openleaderboard, false);
@@ -8,10 +9,15 @@ infolink.addEventListener('click', openinfo, false);
 
 function openrules(e) {
     e.preventDefault();
+    contentbox = document.createElement('div');
+    ruleslink.appendChild(contentbox);
+    contentbox.setAttribute("class", "content-overlay");
 }
 function openleaderboard(e) {
     e.preventDefault();
+
 }
 function openinfo(e) {
     e.preventDefault();
+
 }
