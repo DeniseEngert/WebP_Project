ed095f8495ee695957da4c7b6d2c697b1872f8c9
heineeg <32772232+heineeg@users.noreply.github.com>
2017-12-26 12:25:32 +0100

    * add page_manager.js to offer pages for rules, leaderboard ind info

---
+++ js/page_manager.js
@@ -0,0 +1,11 @@
+var ruleslink = document.getElementById('rules');
+var leaderboardlink = document.getElementById('leaderboard');
+var infolink = document.getElementById('info');
+
+ruleslink.addEventListener('click', openrules, false);
+leaderboardlink.addEventListener('click', openleaderboard, false);
+infolink.addEventListener('click', openinfo, false);
+
+function openrules(){}
+function openleaderboard(){}
+function openinfo(){}
