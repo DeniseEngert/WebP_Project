110d1ad8bda8deefd5590932ea42ef2db16401c6
heineeg <32772232+heineeg@users.noreply.github.com>
2017-12-27 15:29:08 +0100

    * add individual overlay for different functions

--- js/page_manager.js
+++ js/page_manager.js
@@ -14,23 +14,62 @@ function openRules(){
         overlay.removeChild(overlay.lastChild);
     }
     overlay.style.display = "block";
+    var div = document.createElement('div');
     var headlineText = document.createTextNode("Rules");
     var rulesText = document.createTextNode("1. Use keyboard arrows to move the tiles");
     var headline = document.createElement('h1');
     var rulesContainer = document.createElement('p');
     headline.setAttribute("class", "overlay-headline");
     rulesContainer.setAttribute("class", "overlay-text");
+    div.setAttribute("class", "overlay-layout");
     headline.appendChild(headlineText);
     rulesContainer.appendChild(rulesText);
-    overlay.appendChild(headline);
-    overlay.appendChild(rulesContainer);
+    div.appendChild(headline);
+    div.appendChild(rulesContainer);
+    overlay.appendChild(div);
 }
 
 function openLeaderboard() {
     overlay.style.display = "block";
+    while (overlay.hasChildNodes()) {
+        overlay.removeChild(overlay.lastChild);
+    }
+    overlay.style.display = "block";
+    var div = document.createElement('div');
+    var headlineText = document.createTextNode("Leaderboard");
+    var leaderboardText = document.createTextNode("");
+    var headline = document.createElement('h1');
+    var leaderboardContainer = document.createElement('p');
+    headline.setAttribute("class", "overlay-headline");
+    leaderboardContainer.setAttribute("class", "overlay-text");
+    div.setAttribute("class", "overlay-layout");
+    headline.appendChild(headlineText);
+    leaderboardContainer.appendChild(leaderboardText);
+    div.appendChild(headline);
+    div.appendChild(leaderboardContainer);
+    overlay.appendChild(div);
 }
+
 function openInfo() {
     overlay.style.display = "block";
+    overlay.style.display = "block";
+    while (overlay.hasChildNodes()) {
+        overlay.removeChild(overlay.lastChild);
+    }
+    overlay.style.display = "block";
+    var div = document.createElement('div');
+    var headlineText = document.createTextNode("Info");
+    var infoText = document.createTextNode("");
+    var headline = document.createElement('h1');
+    var infoContainer = document.createElement('p');
+    headline.setAttribute("class", "overlay-headline");
+    infoContainer.setAttribute("class", "overlay-text");
+    div.setAttribute("class", "overlay-layout");
+    headline.appendChild(headlineText);
+    infoContainer.appendChild(infoText);
+    div.appendChild(headline);
+    div.appendChild(infoContainer);
+    overlay.appendChild(div);
 }
 
 
