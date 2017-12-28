62a2d3bcb963d21cc04f1aae0804024101ac9e97
heineeg <32772232+heineeg@users.noreply.github.com>
2017-12-28 16:57:44 +0100

    * simplification and leaner code for the overlay elements

--- js/page_manager.js
+++ js/page_manager.js
@@ -2,6 +2,8 @@ var rulesLink = document.getElementById('rules');
 var leaderboardLink = document.getElementById('leaderboard');
 var infoLink = document.getElementById('info');
 var overlay = document.getElementById('overlay');
+var overlayHeadline = document.getElementById('overlay-headline');
+var overlayText = document.getElementById('overlay-text');
 
 
 rulesLink.addEventListener('click', openRules, false);
@@ -10,66 +12,21 @@ infoLink.addEventListener('click', openInfo, false);
 overlay.addEventListener('click', off, false);
 
 function openRules(){
-    while (overlay.hasChildNodes()) {
-        overlay.removeChild(overlay.lastChild);
-    }
     overlay.style.display = "block";
-    var div = document.createElement('div');
-    var headlineText = document.createTextNode("Rules");
-    var rulesText = document.createTextNode("1. Use keyboard arrows to move the tiles");
-    var headline = document.createElement('h1');
-    var rulesContainer = document.createElement('p');
-    headline.setAttribute("class", "overlay-headline");
-    rulesContainer.setAttribute("class", "overlay-text");
-    div.setAttribute("class", "overlay-layout");
-    headline.appendChild(headlineText);
-    rulesContainer.appendChild(rulesText);
-    div.appendChild(headline);
-    div.appendChild(rulesContainer);
-    overlay.appendChild(div);
+    overlayHeadline.textContent = ("Rules");
+    overlayText.textContent = ("1. Move the tiles using the keyboard arrows.");
 }
 
 function openLeaderboard() {
     overlay.style.display = "block";
-    while (overlay.hasChildNodes()) {
-        overlay.removeChild(overlay.lastChild);
-    }
-    overlay.style.display = "block";
-    var div = document.createElement('div');
-    var headlineText = document.createTextNode("Leaderboard");
-    var leaderboardText = document.createTextNode("");
-    var headline = document.createElement('h1');
-    var leaderboardContainer = document.createElement('p');
-    headline.setAttribute("class", "overlay-headline");
-    leaderboardContainer.setAttribute("class", "overlay-text");
-    div.setAttribute("class", "overlay-layout");
-    headline.appendChild(headlineText);
-    leaderboardContainer.appendChild(leaderboardText);
-    div.appendChild(headline);
-    div.appendChild(leaderboardContainer);
-    overlay.appendChild(div);
+    overlayHeadline.textContent = ("Leaderboard");
+    overlayText.textContent = ("");
 }
 
 function openInfo() {
     overlay.style.display = "block";
-    overlay.style.display = "block";
-    while (overlay.hasChildNodes()) {
-        overlay.removeChild(overlay.lastChild);
-    }
-    overlay.style.display = "block";
-    var div = document.createElement('div');
-    var headlineText = document.createTextNode("Info");
-    var infoText = document.createTextNode("");
-    var headline = document.createElement('h1');
-    var infoContainer = document.createElement('p');
-    headline.setAttribute("class", "overlay-headline");
-    infoContainer.setAttribute("class", "overlay-text");
-    div.setAttribute("class", "overlay-layout");
-    headline.appendChild(headlineText);
-    infoContainer.appendChild(infoText);
-    div.appendChild(headline);
-    div.appendChild(infoContainer);
-    overlay.appendChild(div);
+    overlayHeadline.textContent = ("Info");
+    overlayText.textContent = ("");
 }
 
 
