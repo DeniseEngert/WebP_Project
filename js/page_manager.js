d94e6aca2f5ccb6a3cc2387ad8d85815b8f8576a
heineeg <32772232+heineeg@users.noreply.github.com>
2018-01-19 14:14:10 +0100

    * adjustments on rules and add margin to text content

--- js/page_manager.js
+++ js/page_manager.js
@@ -6,12 +6,12 @@ var overlayHeadline = document.getElementById('overlay-headline');
 var overlayText = document.getElementById('overlay-text');
 var closingButton = document.getElementById('closing-button');
 
-var rulesContent = '{"rules":["1. Use the keyboard arrows to move the tiles and join equal numbers.",' +
-    '"2. The objective is to get the number 2048 exponentiating the number two and its multiples.",'+
-    '"3. In the grid of 16 tiles you will initially be given two number twos or a number four.",'+
-    '"4. When two equal numbers are in touch: they will add up.",' +
-    '"5. With every move made the game will provide us another number.",' +
-    '"6. If there are no free tiles on our grid: the game ends."]}';
+var rulesContent = '{"rules":["Use the keyboard arrows to move the tiles and join equal numbers.",' +
+    '"The objective is to get the number 2048 exponentiating the number two and its multiples.",'+
+    '"In the grid of 16 tiles you will initially be given two number twos or a number four.",'+
+    '"When two equal numbers are in touch: they will add up.",' +
+    '"With every move made the game will provide us another number.",' +
+    '"If there are no free tiles on our grid: the game ends."]}';
 
 rulesLink.addEventListener('click', openRules, false);
 leaderboardLink.addEventListener('click', openLeaderboard, false);
