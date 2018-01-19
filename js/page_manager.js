1ab524ce1da71687d94820b88f01037f0d598db2
heineeg <32772232+heineeg@users.noreply.github.com>
2018-01-19 12:08:30 +0100

    * add rules as JSON content

--- js/page_manager.js
+++ js/page_manager.js
@@ -6,7 +6,12 @@ var overlayHeadline = document.getElementById('overlay-headline');
 var overlayText = document.getElementById('overlay-text');
 var closingButton = document.getElementById('closing-button');
 
-var rulesContent = '{"rules":["1. use tabs to navigate", "2....."]}';
+var rulesContent = '{"rules":["1. Use the keyboard arrows to move the tiles and join equal numbers.",' +
+    '"2. The objective is to get the number 2048 exponentiating the number two and its multiples.",'+
+    '"3. In the grid of 16 tiles you will initially be given two number twos or a number four.",'+
+    '"4. When two equal numbers are in touch: they will add up.",' +
+    '"5. With every move made the game will provide us another number.",' +
+    '"6. If there are no free tiles on our grid: the game ends."]}';
 
 rulesLink.addEventListener('click', openRules, false);
 leaderboardLink.addEventListener('click', openLeaderboard, false);
@@ -18,7 +23,7 @@ function openRules(){
     var text = JSON.parse(rulesContent);
     overlayHeadline.textContent = ("Rules");
     overlayText.innerHTML = text.rules;
-    overlayText.innerHTML = overlayText.innerHTML.replace(",","</br>");
+    overlayText.innerHTML = overlayText.innerHTML.replace(/,/g,"<br>");
 }
 
 function openLeaderboard() {
