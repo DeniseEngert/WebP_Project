af9ad78da5b4b694c4308922f364adced330f2f8
heineeg <32772232+heineeg@users.noreply.github.com>
2018-01-19 17:18:18 +0100

    * export text content to contents.js

---
+++ js/contents.js
@@ -0,0 +1,6 @@
+var rulesContent = '{"rules":["Use the keyboard arrows to move the tiles and join equal numbers.",' +
+    '"The objective is to get the number 2048 exponentiating the number two and its multiples.",'+
+    '"In the grid of 16 tiles you will initially be given two number twos or a number four.",'+
+    '"When two equal numbers are in touch: they will add up.",' +
+    '"With every move made the game will provide us another number.",' +
+    '"If there are no free tiles on our grid: the game ends."]}';
--- js/page_manager.js
+++ js/page_manager.js
@@ -6,13 +6,6 @@ var overlayHeadline = document.getElementById('overlay-headline');
 var overlayText = document.getElementById('overlay-text');
 var closingButton = document.getElementById('closing-button');
 
-var rulesContent = '{"rules":["Use the keyboard arrows to move the tiles and join equal numbers.",' +
-    '"The objective is to get the number 2048 exponentiating the number two and its multiples.",'+
-    '"In the grid of 16 tiles you will initially be given two number twos or a number four.",'+
-    '"When two equal numbers are in touch: they will add up.",' +
-    '"With every move made the game will provide us another number.",' +
-    '"If there are no free tiles on our grid: the game ends."]}';
-
 rulesLink.addEventListener('click', openRules, false);
 leaderboardLink.addEventListener('click', openLeaderboard, false);
 infoLink.addEventListener('click', openInfo, false);
