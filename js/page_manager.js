588d4142a16809ab21e3e0eb9aaba21bd9801459
heineeg <32772232+heineeg@users.noreply.github.com>
2018-01-25 11:58:01 +0100

    * add twitterlink to info

--- js/page_manager.js
+++ js/page_manager.js
@@ -33,8 +33,11 @@ function openInfo() {
     overlay.style.display = "block";
     overlayHeadline.textContent = ("Info");
     overlayText.innerHTML = (i.html());
-    /* TODO <a class="twitter-timeline" href="https://twitter.com/gabrielecirulli?ref_src=twsrc%5Etfw">Tweets by gabrielecirulli</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-*/
+    var twitterlink = document.createElement('a');
+    twitterlink.setAttribute("href", "https://twitter.com/gabrielecirulli?ref_src=twsrc%5Etfw");
+    twitterlink.setAttribute("class", "twitter-timeline");
+    twitterlink.textContent = ("Gabriele Cirulli on twitter");
+    overlayText.appendChild(twitterlink);
 }
 
 function openDocumentation() {
