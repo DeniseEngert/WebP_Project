5274bbc36e649048a6eb87bf217060738f7f47ef
heineeg <32772232+heineeg@users.noreply.github.com>
2018-01-26 18:21:44 +0100

    * layout adjustments

--- js/page_manager.js
+++ js/page_manager.js
@@ -60,7 +60,6 @@ function off() {
 }
 
 $(document).ready(function () {
-
     $.ajax({
         type: "GET",
         url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=2048_(video_game)&callback=?",
@@ -68,7 +67,6 @@ $(document).ready(function () {
         async: false,
         dataType: "json",
         success: function (data, textStatus, jqXHR) {
-
             var markup = data.parse.text["*"];
             i = $('<div></div>').html(markup);
             $('#article').html($(blurb).find('p'));
@@ -85,8 +83,6 @@ $(document).ready(function () {
             i.find('.mw-ext-cite-error').remove();
 
             //$('#info').html($(i).find('p'));
-
-
         },
         error: function (errorMessage) {
         }
